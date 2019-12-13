85507e3523a08388d7c30b1865b660f473863067
Kobatai <taiga1983346@gmail.com>
2019-12-13 14:05:13 +0900

    webrootの修正

--- vue-splash/routes/web.php
+++ vue-splash/routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// SPAなのでURL以外のリクエストに対してはindexの画面を返す
+// 画面の遷移はVueRouterが制御
 Route::get('/', function () {
     return view('welcome');
 });
