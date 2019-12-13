8a813b84c56261daac14301fffec0999336d40a6
Kobatai <taiga1983346@gmail.com>
2019-12-13 14:35:05 +0900

    rooting&indexページ作成

--- vue-splash/routes/web.php
+++ vue-splash/routes/web.php
@@ -2,6 +2,8 @@
 
 // SPAなのでURL以外のリクエストに対してはindexの画面を返す
 // 画面の遷移はVueRouterが制御
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any?}', function () {
+	return view('index');
+	// whereでパラメータを正規表現でバリデーション
+	// ここでは+なのでなにが来てもいい
+})->where('any', '.+');
