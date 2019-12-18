af69b04270802693d2d47bfcbdd86656897bbd27
Kobatai <taiga1983346@gmail.com>
2019-12-18 13:57:00 +0900

    ファイルコンポーネントにvalueを渡したら表示するように修正

--- vue-splash/resources/js/components/PhotoForm.vue
+++ vue-splash/resources/js/components/PhotoForm.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="photo-form">
-    <h2 class="title">Submit a photo</h2>
-    <form class="form">
-      <input class="form__item" type="file">
-      <div class="form__button">
-        <button type="submit" class="button button--inverse">submit</button>
-      </div>
-    </form>
-  </div>
+	<div v-show="value" class="photo-form">
+		<div class="photo-form">
+			<h2 class="title">Submit a photo</h2>
+			<form class="form">
+				<input class="form__item" type="file">
+				<div class="form__button">
+					<button type="submit" class="button button--inverse">submit</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		// valueを受け取るため
+		value: {
+			// valueは表示、非表示で表現する
+			type: Boolean,
+			required: true
+		}
+	}
+}
+</script>
