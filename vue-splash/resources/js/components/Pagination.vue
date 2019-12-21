7f15f7ba7709fb4b9af2497d5c9952dc51bac8c0
Kobatai <taiga1983346@gmail.com>
2019-12-21 19:21:52 +0900

    pagerのコンポーネント作成

---
+++ vue-splash/resources/js/components/Pagination.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pagination">
+    <RouterLink
+     v-fi="! isFirstPage"
+     :to="`/?page=${currentPage - 1}`"
+     class="button"
+    >&laquo; prev</RouterLink>
+    <RouterLink
+     v-fi="! isLastPage"
+     :to="`/?page=${currentPage - 1}`"
+     class="button"
+    >&laquo; prev</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    lastPage: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    isFirstPage () {
+      return this.currentPage === 1
+    },
+    isLastPage () {
+      return this.currentPage === this.lastPage
+    }
+  }
+}
+</script>
